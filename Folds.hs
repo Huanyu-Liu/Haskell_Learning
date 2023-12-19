e8baceec85cf38c2332340333eb1f725c75ed04c
Andy Liu <andy.liu@sc.com>
2023-12-19 18:51:10 +0800

    add lecture notes and course 3-5

--- Folds.hs
+++ Folds.hs
@@ -1,23 +1,21 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Folds where
 
-import Prelude hiding (and, elem, map, filter, zip)
+import Prelude hiding (and, elem, map, filter, zip, foldr, flip)
 
 elem :: Eq a => a -> [a] -> Bool
 elem _ [] = False
 elem x (y : ys) = x == y || elem x ys
 
-fun :: (a -> r -> r) -> r -> [a] -> r
-fun cons nil [] = nil
-fun cons nil (x : xs) = cons x (fun cons nil xs)
-
 elem' :: Eq a => a -> [a] -> Bool
-elem' y = fun (\ x r -> x == y || r) False
+elem' y = foldr (\ x r -> x == y || r) False
 
 map' :: (a -> b) -> [a] -> [b]
-map' f = fun (\ x r -> f x : r) []
+map' f = foldr (\ x r -> f x : r) []
 
 and' :: [Bool] -> Bool
-and' = fun (&&) True
+and' = foldr (&&) True
 
 or' :: [Bool] -> Bool
 or' = foldr (||) False
@@ -39,8 +37,50 @@ any' f = foldr (\ x r -> f x || r) False
 
 zip' :: [a] -> [b] -> [(a, b)]
 zip' (x : xs) = foldr (\ y r -> (x, y) : r) []
-zip' _ _ = []
+zip' [] = const []
 
 zip :: [a] -> [b] -> [(a, b)]
 zip (x : xs) (y : ys) = (x, y) : zip xs ys
 zip _ _ = []
+
+foldr :: (a -> r -> r) -> r -> [a] -> r
+foldr cons nil [] = nil
+foldr cons nil (x : xs) = cons x (foldr cons nil xs)
+
+-- foldr cons nil [x, y, z]
+-- = foldr cons nil (x : y : z : [])
+-- = x `cons` (foldr cons nil (y : z : []))
+-- = x `cons` (y `cons` (foldr cons nil (z : [])))
+-- = x `cons` (y `cons` (z `cons` (foldr cons nil [])))
+-- = x `cons` (y `cons` (z `cons` nil))
+
+-- foldl' upd ini [x, y, z]
+-- = foldl' upd init (x : (y : (z : [])))
+-- = go ini (x : (y : (z : [])))
+-- = go (ini `upd` x) (y : (z : []))
+-- = go ((ini `upd` x) `upd` y) (z : [])
+-- = go (((ini `upd` x) `upd` y) `upd` z) []
+-- = (((ini `upd` x) `upd` y) `upd` z)
+
+foldl' :: (r -> a -> r) -> r -> [a] -> r
+foldl' upd ini = go ini
+    where
+        go !acc [] = acc
+        go !acc (x : xs) = go (upd acc x) xs
+
+reverse' :: [a] -> [a]
+reverse' = foldl' (flip (:)) []
+
+flip :: (a -> b -> c) -> b -> a -> c
+flip f a b = f b a
+
+sum' :: Num a => [a] -> a
+sum' = foldl' (+) 0
+
+length' :: [a] -> Int
+length' = foldl' (\ acc _ -> acc + 1) 0
+
+maximum' :: Ord a => [a] -> Maybe a
+maximum' = foldl' (\ acc x -> case acc of
+                                Nothing -> Just x
+                                Just z -> Just (max z x)) Nothing
